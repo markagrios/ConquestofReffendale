e47c498b7b0e221b328a3401fa6d809a6d13034a
Alex Cabrera <alex.cabrera@gmail.com>
2014-09-16 12:24:51 -0400

    Grid made

--- script.js
+++ script.js
@@ -2,6 +2,15 @@ var canvas = document.getElementById("canvas");
 var context = canvas.getContext("2d");
 
 context.beginPath();
-context.rect(0, 0, 600, 300);
+context.rect(0, 0, 800, 500);
 context.fillStyle = '#BADA55';
 context.fill();
+
+for(var x = 5; x < 800; x += 20){
+	for(var y = 5; y < 500; y += 20) {
+		context.beginPath();
+		context.rect(x, y, 10, 10);
+		context.fillStyle = "#000000";
+		context.fill();
+	}
+}
