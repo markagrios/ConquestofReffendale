e38aa30d10d62c6d3a96c5443fb054bae792936c
Alex Cabrera <alex.cabrera@gmail.com>
2014-09-16 12:07:55 -0400

    Canvas set up

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
-alert("hello world");
+var canvas = document.getElementById("canvas");
+var context = canvas.getContext("2d");
+
+context.beginPath();
+context.rect(0, 0, 600, 300);
+context.fillStyle = '#BADA55';
+context.fill();
