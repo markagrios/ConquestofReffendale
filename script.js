bf0006e41b664509b50b4cbd02d916d733576167
Alex Cabrera <alex.cabrera@gmail.com>
2014-09-24 11:50:11 -0400

    twons

--- script.js
+++ script.js
@@ -8,11 +8,12 @@ const RED = 2;
 const REDTILE = 'rgba(200,0,0,.5';
 const BLUETILE = 'rgba(0,0,200,.5)';
 const NEUTRALTILE = 'rgba(0,0,0,0)';
-const DESERT = 1;				// hard to expand, no rm, minimal gold
+const DESERT = 0;				// hard to expand, no rm, minimal gold
 const FOREST = 2;				// hard ish to expand, good rm, some gold
 const GRASS = 4;				// easy to expand, some rm, some gold
 const MOUNTAIN = 8;			    // hard to expand, good rm, good gold
 const WHEAT = 16;				// easy ish to expand, great rm, good ish gold
+const INTERVAL = 500;
 
 var rmoney;
 var bmoney;
@@ -53,14 +54,19 @@ function Building(){
 var map = [];
 for(var i = 0; i < 80; i++){
     map[i] = [];
+    for(var x = 0; x < 50; x++){
+        map[i][x] = new Cell(i, x, NEUTRAL, DESERT);
+    }
 }
 	
 /* PRE: x and y are index positions, color is constant */
 function drawCell(x, y, color) {
+	context.clearRect ( x*10, y*10, 10, 10 );
 	context.beginPath();
 	context.rect(x*10, y*10, CELL_SIZE, CELL_SIZE);
 	context.fillStyle = color;
 	context.fill();
+    context.drawImage(imagesArray[map[x][y].terrain], x*10, y*10);
 }
 
 /* PRE: x and y are index positions, team is a constant */
@@ -84,19 +90,23 @@ function conquerCells(x, y, w, h, team) {
 		}
 	}
 }
+var indexer = 0;
+var drawRed = function() {
+	conquerCell(indexer, indexer, RED);
+	indexer++;
+}
 	
 var main = function(){
 	/* Side bar */
 	context.beginPath();
 	context.rect(800, 0, 100, 500);
-	context.fillStyle = '#000000';
+	context.fillStyle = '#ffffff';
 	context.fill();
 	
 	/* draw initial map */
 	for(var x = 0; x < 80; x++){
 		for(var y = 0; y < 50; y++) {
 	        drawCell(x, y, "rgba(184,138,0,.5)");
-	        map[x][y] = new Cell(x, y, NEUTRAL, DESERT);
 	        context.drawImage(imagesArray[0], x*10, y*10);
 		}
 	}
@@ -118,5 +128,16 @@ var main = function(){
 	conquerCells(50, 10, 10, 20, BLUE);
 	
 	context.drawImage(imagesArray[1], 5, 5);
-	context.drawImage(imagesArray[1], 775, 475)
+	context.drawImage(imagesArray[1], 775, 475);
+
+	context.font = "bold 16px Arial";
+	context.fillStyle = 'black';
+	context.fillText("Buildings", 815, 20);
+	context.beginPath();
+	context.fillStyle = 'blue';
+	context.font = "normal 10px Arial";
+	context.fillText("Town Hall", 840, 42);
+	context.drawImage(imagesArray[1], 815, 30);
+	
+	//window.setInterval(drawRed, INTERVAL);
 }
