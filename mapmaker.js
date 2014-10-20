e5804dabd86403a9eabd093efa526f1a99ac1e54
Mark Agrios <magrios@gmail.com>
2014-10-20 12:29:53 -0400

    jkdfsdcslchjsduich,psiadcis;zkdi

--- mapmaker.js
+++ mapmaker.js
@@ -1,8 +1,9 @@
 var drawMap = function DrawMap() {
 
-const xVal = 0;
-const yVal = 1;
-const tVal = 2;
+const XVAL = 0;
+const YVAL = 1;
+const TVAL = 2;
+
 const DESERT = 1;
 const FOREST = 2;
 const GRASS = 4;
@@ -27,34 +28,33 @@ for(var i = 0; i < 25; i++) {		// need to sort out how many elements will be in 
 
 var enough = 0;
 var icount = 0;
+var t = 0;
+var x = 0;
+var y = 0;
 
-while(enough != 31) {
+while(enough < 31) {
 
-	var t = 0;
+	t = 0;
 
-	var x = (getRandomInt(0,80));	// x and y values for cell being placed
-	var y = (getRandomInt(0,50));
-	var t = 1 << (getRandomInt(0,5));			// sets variable to power of 2
+	x = (getRandomInt(0,80));	// x and y values for cell being placed
+	y = (getRandomInt(0,50));
+	t = 1 << (getRandomInt(0,5));			// sets variable to power of 2
 	
 	enough |= t;									// ors x to enough. eg. if x is 000100 and enough is 010000 then enough |= x   is 010100
 
-	initials[icount][xVal] = x;		// x value
-	initials[icount][yVal] = y;		// y value
-	initials[icount][tVal] = t;		// terrain value
+	initials[icount][XVAL] = x;		// x value
+	
+	initials[icount][YVAL] = y;		// y value
+	initials[icount][TVAL] = t;		// terrain value
 
 	icount++;
-	if(icount > 24) {
-			break;
-	}
-	//console.log(icount);
 }
 
 /*~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~*/
-//console.log(initials[icount - 1][tVal]);
-icount--;
+//console.log(initials[icount - 1][TVAL]);
 /*~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ Places starting cells ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~*/
-for(icount; icount >= 0 ; icount--) {
-	map[initials[icount][xVal]][initials[icount][yVal]] = initials[icount][tVal];
+for(icount--; icount >= 0; icount--) {
+	map[initials[icount][XVAL]][initials[icount][YVAL]] = initials[icount][TVAL];
 }
 /*~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~*/
 
@@ -69,35 +69,37 @@ for(icount; icount >= 0 ; icount--) {
  * Starts at initials block's coordinates, looks at random adjacent cell and if null, fill in with terrain.
  * If not null, move observation point to chosen adjacent cell.
  */
+icount = 0;
 for(var i = 0; i < 100; i++) {			// arbitrary amount I need to define
 	
-	var dx = getRandomInt(-1,1);
-	var dy = getRandomInt(-1,1);
+	var dx = getRandomInt(-1,2);
+	var dy = getRandomInt(-1,2);
 
-	icount = 0;
-	initials[icount][xVal] += dx;	// increment x value of certain block
-	initials[icount][yVal] += dy;	// increment y value of certain block
-	/*while(initials[icount][xVal] < 0 || initials[icount][yVal] < 0 || initials[icount][xVal] > 80 || initials[icount][yVal] > 50) {
+	initials[icount][XVAL] += dx;	// increment x value of certain block
+	initials[icount][YVAL] += dy;	// increment y value of certain block
+	/*while(initials[icount][XVAL] < 0 || initials[icount][YVAL] < 0 || initials[icount][XVAL] > 80 || initials[icount][YVAL] > 50) {
 		var dx = getRandomInt(-1,1);
 		var dy = getRandomInt(-1,1);
 		
-		initials[icount][xVal] += dx;	// increment x value of certain block
-		initials[icount][yVal] += dy;	// increment y value of certain block
+		initials[icount][XVAL] += dx;	// increment x value of certain block
+		initials[icount][YVAL] += dy;	// increment y value of certain block
 	}*/
-	//console.log(initials[icount][xVal]);
-	//console.log(initials[icount][yVal]);
-	console.log(map[initials[icount][xVal]][initials[icount][yVal]]);
-	while(map[initials[icount][xVal]][initials[icount][yVal]] != null) {	//enter loop if cell in occupied		
-		var dx = getRandomInt(-1,1);
-		var dy = getRandomInt(-1,1);
+	console.log(initials[icount][XVAL]);
+	console.log(initials[icount][YVAL]);
+	//console.log(map[initials[icount][XVAL]][initials[icount][YVAL]]);
+	//console.log(map[initials[icount][XVAL]][initials[icount][YVAL]]);
+	while(typeof map[initials[icount][XVAL]][initials[icount][YVAL]] !== "undefined") {	//enter loop if cell exists
 		
-		initials[icount][xVal] += dx;
-		initials[icount][yVal] += dy;
+		var dx = getRandomInt(-1,2);
+		var dy = getRandomInt(-1,2);
+		
+		initials[icount][XVAL] += dx;
+		initials[icount][YVAL] += dy;
 	}
-	map[initials[icount][xVal]][initials[icount][yVal]] = initials[icount][tVal]; //assign terrain value to new cell
+	map[initials[icount][XVAL]][initials[icount][YVAL]] = initials[icount][TVAL]; //assign terrain value to new cell
 	
 	icount++;	
-	if(icount + 1 == null) {				// loops back to first element if the next is null so it keeps cycling through
+	if(icount == null) {				// loops back to first element if the next is null so it keeps cycling through
 		icount = 0;
 	}
 }
