deef23935d06828f1651b807fa4b2a3fcac6c528
Alex Cabrera <alex.cabrera@gmail.com>
2014-11-25 12:14:40 -0500

    alt map default

--- script.js
+++ script.js
@@ -1,5 +1,8 @@
-var canvas = document.getElementById("canvas");
-var context = canvas.getContext("2d");
+var canvasbase = document.getElementById("canvasbase");
+var contextbase = canvasbase.getContext("2d");
+
+//var canvasmenu = document.getElementById("canvasmenu");
+//var contextmenu = canvasmenu.getContext("2d");
 
 const CELL_SIZE = 10;
 
@@ -18,6 +21,8 @@ const GRASS = 4;
 const MOUNTAIN = 8;
 const WHEAT = 16;
 const DEBUGGER = 32;
+
+var menu = false;
 
 //Mapping array indexes to binary multiples
 var numbin = {0:1, 1:2, 2:4, 3:8, 4:16, 5:0, 6:32};
@@ -46,22 +51,33 @@ function Cell(x, y, possession, terrain) {
 	this.terrain = terrain;            // terrain attribute on cell, probably should use binary 000001, 000010 , ... 100000
 }
 
-window.addEventListener( "keypress", doKeyDown, false );
+window.addEventListener("keydown", doKeyDown, false);
 
 function doKeyDown(e){
 	console.log(e.keyCode);
 	if(e.keyCode == 82 || e.keyCode == 114){
 		drawInitMap();
 	}
+	if(e.keyCode == 27) {
+		if(menu){
+			contextmenu.clear();
+		} else {
+			contextmenu.beginPath();
+			contextmenu.rect(0, 0, 900, 500);
+			contextmenu.fillStyle = '#ffffff';
+			contextmenu.fill();
+		}
+		menu = !menu;
+	}
 }
 
 // PRE: x and y are index positions, color is constant (RED, BLUE, NEUTRAL)
 function drawCell(x, y, color) {
-	context.clearRect ( x*CELL_SIZE, y*CELL_SIZE, CELL_SIZE, CELL_SIZE );
-	context.beginPath();
-	context.rect(x*CELL_SIZE, y*CELL_SIZE, CELL_SIZE, CELL_SIZE);
-	context.fillStyle = color;
-	context.fill();
+	contextbase.clearRect ( x*CELL_SIZE, y*CELL_SIZE, CELL_SIZE, CELL_SIZE );
+	contextbase.beginPath();
+	contextbase.rect(x*CELL_SIZE, y*CELL_SIZE, CELL_SIZE, CELL_SIZE);
+	contextbase.fillStyle = color;
+	contextbase.fill();
 }
 
 /* PRE: x and y are index positions, team is a constant */
@@ -91,7 +107,7 @@ function drawInitMap(){
 	for(var x = 0; x < 80; x++){
 		for(var y = 0; y < 50; y++) {
 	        drawCell(x, y, NEUTRALTILE);
-	        context.drawImage(imagesArray[map[x][y]], x*10, y*10);
+	        contextbase.drawImage(imagesArray[map[x][y]], x*10, y*10);
 		}
 	}
 }
@@ -99,21 +115,21 @@ function drawInitMap(){
 var main = function(){
 
 	/* Side bar */
-	context.beginPath();
-	context.rect(0, 0, 900, 500);
-	context.fillStyle = '#ffffff';
-	context.fill();
+	contextbase.beginPath();
+	contextbase.rect(0, 0, 900, 500);
+	contextbase.fillStyle = '#ffffff';
+	contextbase.fill();
 
 	drawInitMap();
     
-	context.font = "bold 16px Arial";
-	context.fillStyle = 'black';
-	context.fillText("Buildings", 815, 20);
-	context.beginPath();
-	context.fillStyle = 'blue';
-	context.font = "normal 10px Arial";
-	context.fillText("Town Hall", 840, 42);
-	context.drawImage(imagesArray[1], 815, 30);
+	contextbase.font = "bold 16px Arial";
+	contextbase.fillStyle = 'black';
+	contextbase.fillText("Buildings", 815, 20);
+	contextbase.beginPath();
+	contextbase.fillStyle = 'blue';
+	contextbase.font = "normal 10px Arial";
+	contextbase.fillText("Town Hall", 840, 42);
+	contextbase.drawImage(imagesArray[1], 815, 30);
     
 	//window.setInterval(drawRed, INTERVAL);
 }
